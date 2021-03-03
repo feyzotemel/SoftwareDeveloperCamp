d084e1caf6ef8d3a990ed7ee3f5549dcf411d29a
feyzotemel <feyzullah.temel@hotmail.com>
2021-03-03 21:17:01 +0200

    15.gün ilk mola

--- Business/Concrate/ProductManager.cs
+++ Business/Concrate/ProductManager.cs
@@ -31,7 +31,7 @@ namespace Business.Concrate
             _categoryService = categoryService;
 
         }
-        [SecuredOperation("product.add")]
+        [SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,5 +1,7 @@
 using Business.Abstract;
 using Business.Concrate;
+using Core.DependencyResolvers;
+using Core.Extensions;
 using Core.Utilities.IoC;
 using Core.Utilities.Security.Encryption;
 using Core.Utilities.Security.JWT;
@@ -8,6 +10,7 @@ using DataAccess.Concrate.EntityFramework;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -56,7 +59,10 @@ namespace WebAPI
                         IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
                     };
                 });
-            ServiceTool.Create(services);
+          services.AddDependencyResolvers(new ICoreModule[] { 
+          new CoreModule()
+
+          });
 
         }
 
